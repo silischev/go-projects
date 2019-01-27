b86b2940350e96851b4c1f85fc70dba9cbdaa6e0
a.silischev <a.silischev@yandex.ru>
2019-01-28 00:17:30 +0300

    modify tree leaf symbols

--- dir_tree/main.go
+++ dir_tree/main.go
@@ -45,28 +45,46 @@ func dirTree(out *os.File, path string, printFiles bool, level int) error {
 
 	//fmt.Println(level)
 
-	if printFiles {
-		if filesCount > 0 {
-			level++
-		} else {
-			level--
+	if filesCount > 0 {
+		level++
+	} else {
+		level--
+	}
+
+	for _, dir := range directories {
+		//currentDirNum := index + 1
+		//fmt.Println("├───"+strings.Repeat("	", level), dir.Name(), "level", level, "files", filesCount, "idx ", currentDirNum)
+
+		if !printFiles && !dir.IsDir() {
+			continue
 		}
 
-		for _, dir := range directories {
-			//currentDirNum := index + 1
-			//fmt.Println("├───"+strings.Repeat("	", level), dir.Name(), "level", level, "files", filesCount, "idx ", currentDirNum)
-			printLines(dir.Name(), level)
+		printLines(dir.Name(), level, filesCount)
 
-			if dir.IsDir() {
-				dirPath := path + string(os.PathSeparator) + dir.Name()
-				err = dirTree(out, dirPath, true, level)
-			}
+		if dir.IsDir() {
+			dirPath := path + string(os.PathSeparator) + dir.Name()
+			err = dirTree(out, dirPath, printFiles, level)
 		}
 	}
 
 	return err
 }
 
-func printLines(dirName string, level int) {
-	fmt.Println("├───"+strings.Repeat("	", level), dirName)
+func printLines(dirName string, level int, filesCount int) {
+	switch level {
+	case 0:
+		fmt.Println("├───"+strings.Repeat("", level), dirName)
+	case 1:
+		fmt.Println(strings.Repeat("\t", level)+"├───", dirName)
+	default:
+		fmt.Println(strings.Repeat("│\t", level)+"├───", dirName)
+	}
+
+	/*if level > 1 {
+		fmt.Println(strings.Repeat("│\t", level) + "├───", dirName)
+	} else if level == 1 {
+		fmt.Println(strings.Repeat("\t", level) + "├───", dirName)
+	} else {
+		fmt.Println("├───" + strings.Repeat("", level), dirName)
+	}*/
 }

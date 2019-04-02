99ce38921e2eb3094b409f2e090f158db1be9427
a.silischev <a.silischev@yandex.ru>
2019-04-02 23:26:08 +0300

    wip add sequential processing

--- function_pipeline/signer.go
+++ function_pipeline/signer.go
@@ -1,3 +1,28 @@
 package main
 
-// сюда писать код
+import "fmt"
+
+func main() {
+	data := "0"
+	fmt.Println(SingleHash(data))
+	fmt.Println(MultiHash(SingleHash(data)))
+}
+
+func SingleHash(data string) string {
+	return DataSignerCrc32(data) + "~" + DataSignerCrc32(DataSignerMd5(data))
+}
+
+func MultiHash(data string) string {
+	steps := []string{"0", "1", "2", "3", "4", "5"}
+	res := ""
+
+	for _, val := range steps {
+		res += DataSignerCrc32(val + data)
+	}
+
+	return res
+}
+
+func CombineResults() {
+	// @TODO
+}

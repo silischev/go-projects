73568756065a76c9be982ac2933fb812bdd9d658
a.silischev <a.silischev@yandex.ru>
2019-04-09 00:49:12 +0300

    wip add goroutines

--- function_pipeline/signer.go
+++ function_pipeline/signer.go
@@ -1,32 +1,60 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"strconv"
+)
 
 func main() {
-	data := "0"
+	//data := []int{0, 1}
+	data := []int{0}
 
-	firstRes := SingleHash(data)
-	secondRes := MultiHash(firstRes)
+	jobs := []job{
+		func(in, out chan interface{}) {
+			for _, val := range data {
+				in <- strconv.Itoa(int(val))
+			}
+		},
+		SingleHash,
+		MultiHash,
+		CombineResults,
+	}
 
-	fmt.Println(firstRes)
-	fmt.Println(secondRes)
+	ExecutePipeline(jobs...)
+
+	fmt.Scanln()
 }
 
-func SingleHash(data string) string {
-	return DataSignerCrc32(data) + "~" + DataSignerCrc32(DataSignerMd5(data))
+func ExecutePipeline(jobs ...job) {
+	in := make(chan interface{})
+	out := make(chan interface{})
+
+	for _, j := range jobs {
+		go j(in, out)
+	}
 }
 
-func MultiHash(data string) string {
+func SingleHash(in, out chan interface{}) {
+	val := <-in
+	tmp := DataSignerCrc32(val.(string)) + "~" + DataSignerCrc32(DataSignerMd5(val.(string)))
+	log.Println(tmp)
+	out <- tmp
+}
+
+func MultiHash(in, out chan interface{}) {
+	inVal := <-out
 	steps := []string{"0", "1", "2", "3", "4", "5"}
 	res := ""
 
 	for _, val := range steps {
-		res += DataSignerCrc32(val + data)
+		res += DataSignerCrc32(val + inVal.(string))
 	}
 
-	return res
+	log.Println(res)
+	out <- res
 }
 
-func CombineResults() {
-	// @TODO
+func CombineResults(in, out chan interface{}) {
+	log.Println(<-out)
 }

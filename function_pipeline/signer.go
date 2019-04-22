f521c5b6230666dfba26c6d2c00ef6e2c5d9b1ea
a.silischev <a.silischev@yandex.ru>
2019-04-23 01:27:51 +0300

    refactor single (add parallel work) + add debug timers

--- function_pipeline/signer.go
+++ function_pipeline/signer.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"sort"
 	"strconv"
 	"sync"
+	"time"
 )
 
 func main() {
@@ -62,24 +64,47 @@ func ExecutePipeline(jobs ...job) {
 
 func SingleHash(in, out chan interface{}) {
 	for val := range in {
-		log.Println("*1*" + " -> val " + strconv.Itoa(val.(int)))
+		start := time.Now()
+		//log.Println("*1*" + " -> val " + strconv.Itoa(val.(int)))
 
-		tmp := DataSignerCrc32(strconv.Itoa(val.(int))) + "~" + DataSignerCrc32(DataSignerMd5(strconv.Itoa(val.(int))))
-		out <- tmp
+		wg := &sync.WaitGroup{}
+		res1 := ""
+		res2 := ""
+
+		wg.Add(1)
+		go func(wg *sync.WaitGroup) {
+			defer wg.Done()
+			res1 = DataSignerCrc32(strconv.Itoa(val.(int)))
+		}(wg)
+
+		wg.Add(1)
+		go func(wg *sync.WaitGroup) {
+			defer wg.Done()
+			res2 = DataSignerCrc32(DataSignerMd5(strconv.Itoa(val.(int))))
+		}(wg)
+
+		wg.Wait()
+
+		end := time.Since(start)
+		log.Println(fmt.Sprintf("******** 1 => %s", end))
+
+		out <- res1 + "~" + res2
 	}
 }
 
 func MultiHash(in, out chan interface{}) {
 	for val := range in {
+		start := time.Now()
+
 		wg := &sync.WaitGroup{}
 		mu := &sync.Mutex{}
 
-		log.Println("*2*" + " -> val " + val.(string))
+		//log.Println("*2*" + " -> val " + val.(string))
 
 		steps := []int{0, 1, 2, 3, 4, 5}
 		var results []string = make([]string, 6)
 
-		log.Println("*2* ->" + "before cycle")
+		//log.Println("*2* ->" + "before cycle")
 		for _, step := range steps {
 			wg.Add(1)
 			go func(wg *sync.WaitGroup, mu *sync.Mutex, st int, results []string) {
@@ -95,10 +120,12 @@ func MultiHash(in, out chan interface{}) {
 		wg.Wait()
 
 		res := ""
-		for key, val := range results {
-			log.Println(strconv.Itoa(key) + " -> " + val)
+		for _, val := range results {
 			res += val
 		}
+
+		end := time.Since(start)
+		log.Println(fmt.Sprintf("******** 2 => %s", end))
 
 		out <- res
 	}
@@ -109,8 +136,13 @@ func CombineResults(in, out chan interface{}) {
 	res := ""
 
 	for val := range in {
-		log.Println("*3*" + " -> val " + val.(string))
+		start := time.Now()
+
+		//log.Println("*3*" + " -> val " + val.(string))
 		values = append(values, val.(string))
+
+		end := time.Since(start)
+		log.Println(fmt.Sprintf("******** 3 => %s", end))
 	}
 
 	sort.Slice(values, func(i, j int) bool {

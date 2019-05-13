4553c305617a1163da539b84694bc1d884e06929
a.silischev <a.silischev@yandex.ru>
2019-05-14 01:17:59 +0300

    parse ast #1

--- codegen/handlers_gen/codegen.go
+++ codegen/handlers_gen/codegen.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"go/ast"
 	"go/parser"
 	"go/token"
 	"log"
@@ -19,4 +20,46 @@ func main() {
 
 	fmt.Fprintln(out, `package `+node.Name.Name)
 	fmt.Fprintln(out)
+
+	fmt.Fprintln(out, `import "net/http"`)
+
+	for _, f := range node.Decls {
+		switch f.(type) {
+		case *ast.FuncDecl:
+			currFunc := f.(*ast.FuncDecl)
+			log.Println(currFunc.Name)
+		}
+
+		g, ok := f.(*ast.GenDecl)
+		if !ok {
+			continue
+		}
+
+		//log.Println(f.(*ast.FuncDecl))
+
+		/* currFunc, ok := f.(*ast.FuncDecl)
+		if !ok {
+			continue
+		} else {
+			log.Println(currFunc.Name)
+		} */
+
+		for _, spec := range g.Specs {
+			currType, ok := spec.(*ast.TypeSpec)
+			if !ok {
+				continue
+			}
+
+			/* currStruct, ok := currType.Type.(*ast.StructType)
+			if !ok {
+				continue
+			} */
+
+			log.Println(currType.Name.Name)
+		}
+	}
+
+	//fmt.Fprintln(`func ServeHTTP(w http.ResponseWriter, req *http.Request) {`)
+	//fmt.Fprintln(``)
+	//fmt.Fprintln(`}`)
 }

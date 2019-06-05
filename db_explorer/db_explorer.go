e10eea970d2ed2dc6d60ddc8a6e463800e1af822
a.silischev <a.silischev@yandex.ru>
2019-06-06 00:42:06 +0300

    add handler + test query

--- db_explorer/db_explorer.go
+++ db_explorer/db_explorer.go
@@ -1,4 +1,43 @@
 package main
 
+import (
+	"database/sql"
+	"fmt"
+	"net/http"
+)
+
+type dbHandler struct {
+	db *sql.DB
+}
+
 // тут вы пишете код
 // обращаю ваше внимание - в этом задании запрещены глобальные переменные
+func NewDbExplorer(db *sql.DB) (*dbHandler, error) {
+	handler := &dbHandler{
+		db: db,
+	}
+
+	return handler, nil
+}
+
+func (h *dbHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	switch req.URL.Path {
+	case "/":
+		h.getTables()
+	}
+}
+
+func (h *dbHandler) getTables() {
+	rows, err := h.db.Query("SHOW TABLES;")
+	if err != nil {
+		panic(err)
+	}
+
+	var tblName string
+
+	defer rows.Close()
+	for rows.Next() {
+		rows.Scan(&tblName)
+		fmt.Println(tblName)
+	}
+}

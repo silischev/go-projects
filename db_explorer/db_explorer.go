4bbe83cc54384919e0ef105c24e8ae962f9d10ab
a.silischev <a.silischev@yandex.ru>
2019-06-08 23:10:12 +0300

    add get tables route

--- db_explorer/db_explorer.go
+++ db_explorer/db_explorer.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
+	"encoding/json"
 	"net/http"
 )
 
 type dbHandler struct {
 	db *sql.DB
+}
+
+type responseData struct {
+	Response map[string]interface{} `json:"response"`
 }
 
 // тут вы пишете код
@@ -21,23 +25,40 @@ func NewDbExplorer(db *sql.DB) (*dbHandler, error) {
 }
 
 func (h *dbHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	res := ""
+
 	switch req.URL.Path {
 	case "/":
-		h.getTables()
+		res = h.getTables()
 	}
+
+	w.Write([]byte(res))
 }
 
-func (h *dbHandler) getTables() {
+func (h *dbHandler) getTables() string {
+	r := responseData{}
+	r.Response = make(map[string]interface{})
+
 	rows, err := h.db.Query("SHOW TABLES;")
 	if err != nil {
 		panic(err)
 	}
 
 	var tblName string
+	var tblNames []string
 
 	defer rows.Close()
 	for rows.Next() {
 		rows.Scan(&tblName)
-		fmt.Println(tblName)
+		tblNames = append(tblNames, tblName)
 	}
+
+	r.Response["tables"] = tblNames
+
+	result, _ := json.Marshal(r)
+	/* fmt.Println(string(result))
+	log.Println(r)
+	log.Fatal("***") */
+
+	return string(result)
 }

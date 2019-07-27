27af988450c4c15439529c01f2eaa732eeb917ca
a.silischev <a.silischev@yandex.ru>
2019-07-28 00:12:06 +0300

    generate proto (add services)

--- microservice/service.pb.go
+++ microservice/service.pb.go
@@ -4,8 +4,12 @@
 package main
 
 import (
+	context "context"
 	fmt "fmt"
 	proto "github.com/golang/protobuf/proto"
+	grpc "google.golang.org/grpc"
+	codes "google.golang.org/grpc/codes"
+	status "google.golang.org/grpc/status"
 	math "math"
 )
 
@@ -254,4 +258,318 @@ var fileDescriptor_a0b84a42fa06f626 = []byte{
 	0xf0, 0x2d, 0x38, 0xb3, 0xa2, 0xb8, 0x39, 0xf6, 0x0e, 0xdc, 0x5f, 0x44, 0xaa, 0x5b, 0x73, 0xd9,
 	0xbd, 0xf9, 0xe9, 0x0f, 0xff, 0x02, 0x00, 0x00, 0xff, 0xff, 0x03, 0x1d, 0xb2, 0x19, 0xe4, 0x02,
 	0x00, 0x00,
+}
+
+// Reference imports to suppress errors if they are not otherwise used.
+var _ context.Context
+var _ grpc.ClientConn
+
+// This is a compile-time assertion to ensure that this generated file
+// is compatible with the grpc package it is being compiled against.
+const _ = grpc.SupportPackageIsVersion4
+
+// AdminClient is the client API for Admin service.
+//
+// For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
+type AdminClient interface {
+	Logging(ctx context.Context, in *Nothing, opts ...grpc.CallOption) (Admin_LoggingClient, error)
+	Statistics(ctx context.Context, in *StatInterval, opts ...grpc.CallOption) (Admin_StatisticsClient, error)
+}
+
+type adminClient struct {
+	cc *grpc.ClientConn
+}
+
+func NewAdminClient(cc *grpc.ClientConn) AdminClient {
+	return &adminClient{cc}
+}
+
+func (c *adminClient) Logging(ctx context.Context, in *Nothing, opts ...grpc.CallOption) (Admin_LoggingClient, error) {
+	stream, err := c.cc.NewStream(ctx, &_Admin_serviceDesc.Streams[0], "/main.Admin/Logging", opts...)
+	if err != nil {
+		return nil, err
+	}
+	x := &adminLoggingClient{stream}
+	if err := x.ClientStream.SendMsg(in); err != nil {
+		return nil, err
+	}
+	if err := x.ClientStream.CloseSend(); err != nil {
+		return nil, err
+	}
+	return x, nil
+}
+
+type Admin_LoggingClient interface {
+	Recv() (*Event, error)
+	grpc.ClientStream
+}
+
+type adminLoggingClient struct {
+	grpc.ClientStream
+}
+
+func (x *adminLoggingClient) Recv() (*Event, error) {
+	m := new(Event)
+	if err := x.ClientStream.RecvMsg(m); err != nil {
+		return nil, err
+	}
+	return m, nil
+}
+
+func (c *adminClient) Statistics(ctx context.Context, in *StatInterval, opts ...grpc.CallOption) (Admin_StatisticsClient, error) {
+	stream, err := c.cc.NewStream(ctx, &_Admin_serviceDesc.Streams[1], "/main.Admin/Statistics", opts...)
+	if err != nil {
+		return nil, err
+	}
+	x := &adminStatisticsClient{stream}
+	if err := x.ClientStream.SendMsg(in); err != nil {
+		return nil, err
+	}
+	if err := x.ClientStream.CloseSend(); err != nil {
+		return nil, err
+	}
+	return x, nil
+}
+
+type Admin_StatisticsClient interface {
+	Recv() (*Stat, error)
+	grpc.ClientStream
+}
+
+type adminStatisticsClient struct {
+	grpc.ClientStream
+}
+
+func (x *adminStatisticsClient) Recv() (*Stat, error) {
+	m := new(Stat)
+	if err := x.ClientStream.RecvMsg(m); err != nil {
+		return nil, err
+	}
+	return m, nil
+}
+
+// AdminServer is the server API for Admin service.
+type AdminServer interface {
+	Logging(*Nothing, Admin_LoggingServer) error
+	Statistics(*StatInterval, Admin_StatisticsServer) error
+}
+
+// UnimplementedAdminServer can be embedded to have forward compatible implementations.
+type UnimplementedAdminServer struct {
+}
+
+func (*UnimplementedAdminServer) Logging(req *Nothing, srv Admin_LoggingServer) error {
+	return status.Errorf(codes.Unimplemented, "method Logging not implemented")
+}
+func (*UnimplementedAdminServer) Statistics(req *StatInterval, srv Admin_StatisticsServer) error {
+	return status.Errorf(codes.Unimplemented, "method Statistics not implemented")
+}
+
+func RegisterAdminServer(s *grpc.Server, srv AdminServer) {
+	s.RegisterService(&_Admin_serviceDesc, srv)
+}
+
+func _Admin_Logging_Handler(srv interface{}, stream grpc.ServerStream) error {
+	m := new(Nothing)
+	if err := stream.RecvMsg(m); err != nil {
+		return err
+	}
+	return srv.(AdminServer).Logging(m, &adminLoggingServer{stream})
+}
+
+type Admin_LoggingServer interface {
+	Send(*Event) error
+	grpc.ServerStream
+}
+
+type adminLoggingServer struct {
+	grpc.ServerStream
+}
+
+func (x *adminLoggingServer) Send(m *Event) error {
+	return x.ServerStream.SendMsg(m)
+}
+
+func _Admin_Statistics_Handler(srv interface{}, stream grpc.ServerStream) error {
+	m := new(StatInterval)
+	if err := stream.RecvMsg(m); err != nil {
+		return err
+	}
+	return srv.(AdminServer).Statistics(m, &adminStatisticsServer{stream})
+}
+
+type Admin_StatisticsServer interface {
+	Send(*Stat) error
+	grpc.ServerStream
+}
+
+type adminStatisticsServer struct {
+	grpc.ServerStream
+}
+
+func (x *adminStatisticsServer) Send(m *Stat) error {
+	return x.ServerStream.SendMsg(m)
+}
+
+var _Admin_serviceDesc = grpc.ServiceDesc{
+	ServiceName: "main.Admin",
+	HandlerType: (*AdminServer)(nil),
+	Methods:     []grpc.MethodDesc{},
+	Streams: []grpc.StreamDesc{
+		{
+			StreamName:    "Logging",
+			Handler:       _Admin_Logging_Handler,
+			ServerStreams: true,
+		},
+		{
+			StreamName:    "Statistics",
+			Handler:       _Admin_Statistics_Handler,
+			ServerStreams: true,
+		},
+	},
+	Metadata: "service.proto",
+}
+
+// BizClient is the client API for Biz service.
+//
+// For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
+type BizClient interface {
+	Check(ctx context.Context, in *Nothing, opts ...grpc.CallOption) (*Nothing, error)
+	Add(ctx context.Context, in *Nothing, opts ...grpc.CallOption) (*Nothing, error)
+	Test(ctx context.Context, in *Nothing, opts ...grpc.CallOption) (*Nothing, error)
+}
+
+type bizClient struct {
+	cc *grpc.ClientConn
+}
+
+func NewBizClient(cc *grpc.ClientConn) BizClient {
+	return &bizClient{cc}
+}
+
+func (c *bizClient) Check(ctx context.Context, in *Nothing, opts ...grpc.CallOption) (*Nothing, error) {
+	out := new(Nothing)
+	err := c.cc.Invoke(ctx, "/main.Biz/Check", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *bizClient) Add(ctx context.Context, in *Nothing, opts ...grpc.CallOption) (*Nothing, error) {
+	out := new(Nothing)
+	err := c.cc.Invoke(ctx, "/main.Biz/Add", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *bizClient) Test(ctx context.Context, in *Nothing, opts ...grpc.CallOption) (*Nothing, error) {
+	out := new(Nothing)
+	err := c.cc.Invoke(ctx, "/main.Biz/Test", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+// BizServer is the server API for Biz service.
+type BizServer interface {
+	Check(context.Context, *Nothing) (*Nothing, error)
+	Add(context.Context, *Nothing) (*Nothing, error)
+	Test(context.Context, *Nothing) (*Nothing, error)
+}
+
+// UnimplementedBizServer can be embedded to have forward compatible implementations.
+type UnimplementedBizServer struct {
+}
+
+func (*UnimplementedBizServer) Check(ctx context.Context, req *Nothing) (*Nothing, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method Check not implemented")
+}
+func (*UnimplementedBizServer) Add(ctx context.Context, req *Nothing) (*Nothing, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method Add not implemented")
+}
+func (*UnimplementedBizServer) Test(ctx context.Context, req *Nothing) (*Nothing, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method Test not implemented")
+}
+
+func RegisterBizServer(s *grpc.Server, srv BizServer) {
+	s.RegisterService(&_Biz_serviceDesc, srv)
+}
+
+func _Biz_Check_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(Nothing)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(BizServer).Check(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/main.Biz/Check",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(BizServer).Check(ctx, req.(*Nothing))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _Biz_Add_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(Nothing)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(BizServer).Add(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/main.Biz/Add",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(BizServer).Add(ctx, req.(*Nothing))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _Biz_Test_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(Nothing)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(BizServer).Test(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/main.Biz/Test",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(BizServer).Test(ctx, req.(*Nothing))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+var _Biz_serviceDesc = grpc.ServiceDesc{
+	ServiceName: "main.Biz",
+	HandlerType: (*BizServer)(nil),
+	Methods: []grpc.MethodDesc{
+		{
+			MethodName: "Check",
+			Handler:    _Biz_Check_Handler,
+		},
+		{
+			MethodName: "Add",
+			Handler:    _Biz_Add_Handler,
+		},
+		{
+			MethodName: "Test",
+			Handler:    _Biz_Test_Handler,
+		},
+	},
+	Streams:  []grpc.StreamDesc{},
+	Metadata: "service.proto",
 }
